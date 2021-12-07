90c45b32963231af760b89dc3820e7c2ea3262ce
MOHAMED RIAD <48014410+mriiad@users.noreply.github.com>
2021-12-07 22:54:06 +0100

    Solution of TwoNumberSum problem

--- TwoNumberSum/solution1.java
+++ TwoNumberSum/solution1.java
@@ -1,5 +1,10 @@
 import java.util.*;
 
+/**
+* @author mriad
+*
+* Problem: return an array of two integers which their sum is equal to a target integer
+*/
 class Program {
     public static int[] twoNumberSum(int[] array, int targetSum) {
         for (int i = 0; i < array.length - 1; i++) {
